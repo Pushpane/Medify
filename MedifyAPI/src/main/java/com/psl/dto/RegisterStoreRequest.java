a0e4cd34f27c8409f8780a576f4be6b15305c7d4
Swaralihb <swarali.hb1@gmail.com>
2021-08-03 11:41:43 +0530

    User service corrected

--- MedifyAPI/src/main/java/com/psl/dto/RegisterStoreRequest.java
+++ MedifyAPI/src/main/java/com/psl/dto/RegisterStoreRequest.java
@@ -8,16 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterStoreRequest {
-<<<<<<< HEAD
-	
-	private String user;
-	private String name;
-	private String description;
-
-=======
 
 	private String email;
 	private String name;
 	private String description;
->>>>>>> 282b6e385d5fd8cda76fdfdf83ea19a301da8e72
+
 }
