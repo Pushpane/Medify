0f4a123145d6473e50a77c35a0d8e8e513497ab4
Jay Dixit <jaysdixit100@gmail.com>
2021-08-04 20:14:44 +0530

    MedicineToStore
    Added MedicineToStore Controller

---
+++ MedifyAPI/src/main/java/com/psl/controller/MedicineToStoreController.java
@@ -0,0 +1,43 @@
+package com.psl.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.psl.dto.RegisterMedicineToStoreRequest;
+import com.psl.service.MedicineToStoreService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/user")
+public class MedicineToStoreController {
+	
+	private final MedicineToStoreService medicineToStoreService;
+	
+	@PostMapping("/addMedicineToStore")
+	public ResponseEntity<HttpStatus> registerMedicineToStore(RegisterMedicineToStoreRequest registerMedicineToStoreRequest) {
+		medicineToStoreService.registerMedicineToStore(registerMedicineToStoreRequest);
+		
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteMedicineToStore/{id}")
+	public ResponseEntity<HttpStatus> deleteMedicineToStore(@PathVariable long id){
+		medicineToStoreService.deleteMedicineToStore(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteMedicineByStore/{id}")
+	public ResponseEntity<HttpStatus> deleteByStoreId(@PathVariable long id){
+		medicineToStoreService.deleteByStoreId(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+	}
+	
+	
+}
---
+++ MedifyAPI/src/main/java/com/psl/service/MedicineToStoreService.java
@@ -0,0 +1,69 @@
+package com.psl.service;
+
+import java.lang.StackWalker.Option;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.psl.dao.IMedicineToStoreDAO;
+import com.psl.dto.RegisterMedicineToStoreRequest;
+import com.psl.entity.MedicineToStore;
+//import com.psl.entity.Medicines;
+import com.psl.entity.Store;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class MedicineToStoreService {
+	
+	private IMedicineToStoreDAO medicineToStoreDAO;
+	private final StoreService storeService;
+	//private final MedicineService medicineService;
+	
+	public void registerMedicineToStore(RegisterMedicineToStoreRequest request) {
+		MedicineToStore medToStore = fillMedicineToStore(request);
+		
+		medicineToStoreDAO.save(medToStore);
+	}
+	
+	private MedicineToStore fillMedicineToStore(RegisterMedicineToStoreRequest request) {
+		MedicineToStore medToStoreEntity = new MedicineToStore();
+		
+		
+		//Remove Comment After adding MedicineService
+//		Optional<Medicines> medicine = null;
+//		if(request.getMedicineName()!=null) {
+//			medicine = medicineService.findMedicineByName(request.getMedicineName())
+//			
+//		}
+		
+		Optional<Store> store = storeService.findStoreByName(request.getStoreName());
+//		if(request.getStoreName()!=null) {
+//			store = storeService.findStoreByName(request.getStoreName());
+//		}
+		
+		medToStoreEntity.setMedicineId(null);
+		medToStoreEntity.setStoreId(null);;
+		medToStoreEntity.setAvailable(true);
+		
+		return medToStoreEntity;
+	}
+	
+	public void deleteMedicineToStore(long id) {
+		medicineToStoreDAO.deleteById(id);
+	}
+	
+	public void deleteByStoreId(long id) {
+//		Optional<Store> store = null;
+//		if(request.getStoreName()!=null) {
+//			store = storeService.findStoreByName(request.getStoreName());
+//		}
+		
+		medicineToStoreDAO.deleteByStoreId(id);
+	}
+	
+}
