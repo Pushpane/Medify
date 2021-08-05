3bb8a68d92164d15e4f1e51a519384c381f42caa
Mitali Lamkhade <mitalilamkhade@gmail.com>
2021-08-05 14:39:28 +0530

    Medicine CRUD added

--- MedifyAPI/src/main/java/com/psl/dao/IMedicinesDAO.java
+++ MedifyAPI/src/main/java/com/psl/dao/IMedicinesDAO.java
@@ -1,5 +1,7 @@
 package com.psl.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.psl.entity.Medicines;
 
 @Repository
 public interface IMedicinesDAO extends JpaRepository<Medicines, Long> {
+
+	Optional<Medicines> findByName(String medicine);
 
 }
