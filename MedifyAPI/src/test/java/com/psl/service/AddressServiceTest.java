e3f1222f65d76053b55d876dc7d7ef9e134dbd86
Mitali Lamkhade <mitalilamkhade@gmail.com>
2021-08-17 19:42:53 +0530

    store address test updated

--- MedifyAPI/src/test/java/com/psl/service/AddressServiceTest.java
+++ MedifyAPI/src/test/java/com/psl/service/AddressServiceTest.java
@@ -1,6 +1,8 @@
 package com.psl.service;
 
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -15,6 +17,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.runner.RunWith;
+import org.mockito.MockedStatic.Verification;
+import org.mockito.exceptions.verification.opentest4j.ArgumentsAreDifferent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -29,40 +33,86 @@ import com.psl.entity.Store;
 import com.psl.entity.User;
 import com.psl.exception.MedifyException;
 
+import junit.framework.AssertionFailedError;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class AddressServiceTest {
-	
-	
+
+
 	@Autowired
 	private AddressService addressService;
-	
+
 	@MockBean
 	private IAddressDAO addressRepository;
-	
+
 	@MockBean
 	private UserService userService;
-	
+
 	@MockBean
 	private StoreService storeService;
-	
+
 	@Test
 	void testRegisterAddress() {
-		
+
 		Role role = new Role(2L, "Owner");
 		User user = new User(2L, "UserName", "UserName1@email.com", "Password", role, "1234567890", null, true);
 		Store store = new Store(1, user, "StoreName", "StoreDescription");
 		Address address = new Address(0,user,store,"address1","address2","pincode","city","state");
-		
-		
+
+
 		RegisterAddressRequest addressRequest = new RegisterAddressRequest("UserName1@email.com",1,"address1","address2","pincode","city","state"); 
 		when(addressRepository.save(address)).thenReturn(address);
 		when(userService.getUser(addressRequest.getEmail())).thenReturn(Optional.of(user));
 		when(storeService.getStoreById(addressRequest.getStoreId())).thenReturn(store);
-		
+
 		addressService.registerAddress(addressRequest);
 		verify(addressRepository,times(1)).save(address);	
-	
+
+	}
+
+	@Test
+	void testRegisterAddressUserNullBranch() {
+		Assertions.assertThrows(NullPointerException.class, new Executable() {
+			Role role = new Role(2L, "Owner");
+			User user = new User(2L, "UserName", "UserName1@email.com", "Password", role, "1234567890", null, true);
+			Store store = new Store(1, user, "StoreName", "StoreDescription");
+			Address address = new Address(0,user,store,"address1","address2","pincode","city","state");
+
+
+			RegisterAddressRequest addressRequest = new RegisterAddressRequest(null,1,"address1","address2","pincode","city","state");
+			@Override
+			public void execute() throws Throwable {
+				when(addressRepository.save(address)).thenReturn(address);
+				when(userService.getUser(addressRequest.getEmail())).thenReturn(Optional.of(user));
+				when(storeService.getStoreById(addressRequest.getStoreId())).thenReturn(store);
+
+				addressService.registerAddress(addressRequest);
+				verify(addressRepository,times(1)).save(address);	
+			}
+		});
+
+	}
+
+	@Test
+	void testRegisterAddressStoreIdZeroBranch() {
+		Assertions.assertThrows(ArgumentsAreDifferent.class, new Executable() {
+			Role role = new Role(2L, "Owner");
+			User user = new User(2L, "UserName", "UserName1@email.com", "Password", role, "1234567890", null, true);
+			Store store = new Store(1, user, "StoreName", "StoreDescription");
+			Address address = new Address(0,user,store,"address1","address2","pincode","city","state");
+
+
+			RegisterAddressRequest addressRequest = new RegisterAddressRequest("UserName1@email.com",1,"address1","address2","pincode","city","state");@Override
+			public void execute() throws Throwable {
+				when(addressRepository.save(address)).thenReturn(address);
+				when(userService.getUser(addressRequest.getEmail())).thenReturn(Optional.of(user));
+				when(storeService.getStoreById(0)).thenReturn(store);
+				addressService.registerAddress(addressRequest);
+				verify(addressRepository,times(1)).save(address);	
+			}
+		});
+
 	}
 
 	@Test
@@ -71,7 +121,7 @@ class AddressServiceTest {
 		User userId = new User(1L,"name","email","password",roleId,"phoneNumber",null,true);
 		Store storeId = new Store(1L,userId,"name","description");
 		Address address = new Address(1L,userId,storeId,"address1","address2","pincode","city","state");
-		
+
 		addressService.updateAddress(address);
 		verify(addressRepository, times(1)).save(address);
 	}
@@ -88,12 +138,12 @@ class AddressServiceTest {
 		User userId1 = new User(1L,"name","email","password",roleId1,"phoneNumber",null,true);
 		Store storeId1 = new Store(1L,userId1,"name","description");
 		Address address1 = new Address(1L,userId1,storeId1,"address1","address2","pincode","city","state");
-		
+
 		Role roleId2 = new Role(2L,"role");
 		User userId2 = new User(2L,"name","email","password",roleId2,"phoneNumber",null,true);
 		Store storeId2 = new Store(2L,userId2,"name","description");
 		Address address2 = new Address(2L,userId2,storeId2,"address1","address2","pincode","city","state");
-		
+
 		when(addressRepository.findAll()).thenReturn(Stream.of( address1, address2).collect(Collectors.toList()));
 		assertEquals(2, addressService.getAllAddress().size());
 	}
@@ -103,17 +153,17 @@ class AddressServiceTest {
 		User userId = new User(1L,"name","email","password",roleId,"phoneNumber",null,true);
 		Store storeId = new Store(1L,userId,"name","description");
 		Address address = new Address(1L,userId,storeId,"address1","address2","pincode","city","state");
-		
+
 		String city = "city";
 		when(addressRepository.findByCity(city)).thenReturn(Stream.of(address).collect(Collectors.toList()));
 		//when(addressRepository.save()).thenReturn(Stream.of(address).collect(Collectors.toList()));
-		
+
 	}
 	@Test
 	void testFindByCity() {
-		 String city = "city";
-		 List<Address> found = addressService.getByCity(city);
-		 assertEquals(found.size(), addressService.getByCity(city).size());
+		String city = "city";
+		List<Address> found = addressService.getByCity(city);
+		assertEquals(found.size(), addressService.getByCity(city).size());
 	}
 
 	@Test
@@ -122,7 +172,7 @@ class AddressServiceTest {
 		User userId = new User(1L,"name","email","password",roleId,"phoneNumber",null,true);
 		Store storeId = new Store(1L,userId,"name","description");
 		Address address = new Address(1L,userId,storeId,"address1","address2","pincode","city","state");
-		
+
 		String pincode = "pincode";
 		when(addressRepository.findByPincode(pincode)).thenReturn(Stream.of(address).collect(Collectors.toList()));
 		assertEquals(1, addressService.getByPincode(pincode).size());
@@ -134,11 +184,11 @@ class AddressServiceTest {
 		User userId = new User(1L,"name","email","password",roleId,"phoneNumber",null,true);
 		Store storeId = new Store(1L,userId,"name","description");
 		Address address = new Address(1L,userId,storeId,"address1","address2","pincode","city","state");
-		
-		
+
+
 		when(addressRepository.findByStoreId(storeId)).thenReturn(address);
 		assertEquals(address, addressService.findByStore(storeId));	
-		
+
 	}
 	@Test
 	void testFindByUser() {
@@ -147,7 +197,7 @@ class AddressServiceTest {
 		Store storeId = new Store(1L,userId,"name","description");
 		Address address1 = new Address(1L,userId,storeId,"address1","address2","pincode","city","state");
 		Address address2 = new Address(2L,userId,storeId,"address1","address2","pincode","city","state");
-		
+
 		when(userService.getUser(userId.getEmail())).thenReturn(Optional.of(userId));
 		when(addressRepository.findByUserId(userId)).thenReturn(Stream.of(address1,address2).collect(Collectors.toList()));
 		assertEquals(2,addressService.findByUser(userId.getEmail()).size());
@@ -155,25 +205,21 @@ class AddressServiceTest {
 	@Test
 	void testFindByUserException() {
 		Assertions.assertThrows(MedifyException.class, new Executable() {
-			
-		Role roleId = new Role(1L,"role");
-		User userId = new User(1L,"name","email","password",roleId,"phoneNumber",null,true);
-		Store storeId = new Store(1L,userId,"name","description");
-		Address address1 = new Address(1L,userId,storeId,"address1","address2","pincode","city","state");
-		Address address2 = new Address(2L,userId,storeId,"address1","address2","pincode","city","state");
-		
-		
-		@Override
-		public void execute() throws Throwable {
-			when(userService.getUser(userId.getEmail())).thenReturn(Optional.of(userId));
-			when(addressRepository.findByUserId(userId)).thenReturn(Stream.of(address1,address2).collect(Collectors.toList()));
-			assertEquals(2,addressService.findByUser("email1").size());
-						
-		}
-		
-		
+
+			Role roleId = new Role(1L,"role");
+			User userId = new User(1L,"name","email","password",roleId,"phoneNumber",null,true);
+			Store storeId = new Store(1L,userId,"name","description");
+			Address address1 = new Address(1L,userId,storeId,"address1","address2","pincode","city","state");
+			Address address2 = new Address(2L,userId,storeId,"address1","address2","pincode","city","state");
+
+			@Override
+			public void execute() throws Throwable {
+				when(userService.getUser(userId.getEmail())).thenReturn(Optional.of(userId));
+				when(addressRepository.findByUserId(userId)).thenReturn(Stream.of(address1,address2).collect(Collectors.toList()));
+				assertEquals(2,addressService.findByUser("email1").size());
+			}
 		});
-		
+
 	}
 	@Test
 	void testGetAddressById() {
@@ -181,14 +227,12 @@ class AddressServiceTest {
 		User userId = new User(1L,"name","email","password",roleId,"phoneNumber",null,true);
 		Store storeId = new Store(1L,userId,"name","description");
 		Address address = new Address(1L,userId,storeId,"address1","address2","pincode","city","state");
-		
-		
 		when(addressRepository.findById(1L)).thenReturn(Optional.of(address));
 		assertEquals(address, addressService.getAddressById(1L));
 	}
-	
-	
-	
+
+
+
 }
 
 
--- MedifyAPI/src/test/java/com/psl/service/StoreServiceTest.java
+++ MedifyAPI/src/test/java/com/psl/service/StoreServiceTest.java
@@ -27,21 +27,21 @@ import com.psl.exception.MedifyException;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class StoreServiceTest {
-	
+
 	@Autowired
 	private StoreService storeService;
-	
+
 	@MockBean
 	private IStoreDAO repository;
 	@MockBean
 	private UserService userService;
-	
+
 	@Test
 	public void registerStoreTest() {
 		Role role = new Role(2L, "Owner");
 		User user = new User(2L, "UserName", "UserName1@email.com", "Password", role, "1234567890", null, true);
 		Store store = new Store(0L, user, "StoreName", "StoreDescription");
-		
+
 		RegisterStoreRequest request = new RegisterStoreRequest("UserName1@email.com", "StoreName", "StoreDescription");
 		when(repository.save(store)).thenReturn(store);
 		when(userService.getUser(request.getEmail())).thenReturn(Optional.of(user));
@@ -50,13 +50,33 @@ class StoreServiceTest {
 		storeService.registerStore(request);
 		verify(repository,times(1)).save(store);
 	}
-	
+
+	@Test
+	public void registerStoreTestEmailNullBranch() {
+		Assertions.assertThrows(NullPointerException.class, new Executable() {
+			Role role = new Role(2L, "Owner");
+			User user = new User(2L, "UserName", "UserName1@email.com", "Password", role, "1234567890", null, true);
+			Store store = new Store(0L, user, "StoreName", "StoreDescription");
+
+			RegisterStoreRequest request = new RegisterStoreRequest(null, "StoreName", "StoreDescription");
+			@Override
+			public void execute() throws Throwable {
+				when(repository.save(store)).thenReturn(store);
+				when(userService.getUser(request.getEmail())).thenReturn(Optional.of(user));
+
+				//Assertions.assertThrows(NoSuchElementException.class, () -> storeService.registerStore(request));
+				storeService.registerStore(request);
+				verify(repository,times(1)).save(store);
+			}
+		});
+	}
+
 	@Test
 	public void findStoreByNameTest() {
 		Role role1 = new Role(1L, "Owner");
 		User user1 = new User(1L, "UserName1", "UserName1@email.com", "Password", role1, "1234567890", null, true);
 		Optional<Store> store1 = Optional.ofNullable(new Store(1L, user1, "StoreName1", "StoreDescription"));
-		
+
 		String storeName = "StoreName1";
 		when(repository.findByName(storeName)).thenReturn((store1));
 		//when(repository.findByName(storeName)).thenReturn(Optional.ofNullable(Stream.of( store1, store2).collect(Collector<Store, A, R>)));
@@ -70,9 +90,9 @@ class StoreServiceTest {
 		Role role = new Role(1L, "Owner");
 		User user = new User(1L, "UserName", "UserName@email.com", "Password", role, "1234567890", null, true);
 		Store store = new Store(1L, user, "StoreName", "StoreDescription");
-		
-//		when(repository.save(store)).thenReturn(store);
-//		assertEquals(store, storeService.updateStore(store));
+
+		//		when(repository.save(store)).thenReturn(store);
+		//		assertEquals(store, storeService.updateStore(store));
 		storeService.updateStore(store);
 		verify(repository, times(1)).save(store);
 	}
@@ -82,7 +102,7 @@ class StoreServiceTest {
 		storeService.deleteStore(1L);
 		verify(repository, times(1)).deleteById(1L);
 	}
-	
+
 	@Test
 	public void getStoreByIdTest() {
 		Role role = new Role(1L, "Owner");
@@ -91,28 +111,28 @@ class StoreServiceTest {
 		when(repository.findById(1L)).thenReturn(Optional.of(store));
 		assertEquals(store, storeService.getStoreById(1L));
 	}
-	
+
 	@Test
 	public void getAllStoresTest() {
 		Role role1 = new Role(1L, "Owner");
 		User user1 = new User(1L, "UserName1", "UserName1@email.com", "Password", role1, "1234567890", null, true);
 		Store store1 = new Store(1L, user1, "StoreName1", "StoreDescription");
-		
+
 		Role role2 = new Role(2L, "Owner");
 		User user2 = new User(2L, "UserName2", "UserName2@email.com", "Password", role2, "1234567890", null, true);
 		Store store2 = new Store(2L, user2, "StoreName2", "StoreDescription");
-		
+
 		when(repository.findAll()).thenReturn(Stream.of( store1, store2).collect(Collectors.toList()));
 		assertEquals(2, storeService.getAllStores().size());
 	}
-	
+
 	@Test
 	public void findStoreByUserTest() {
 		Role role = new Role(1L, "Owner");
 		User user = new User(1L, "UserName", "UserName@email.com", "Password", role, "1234567890", null, true);
 		Store store1 = new Store(1L, user, "StoreName", "StoreDescription");
 		Store store2 = new Store(2L, user, "StoreName", "StoreDescription");
-		
+
 		when(userService.getUser(user.getEmail())).thenReturn(Optional.of(user));
 		when(storeService.findStoreByUser(user.getEmail())).thenReturn(Stream.of(store1,store2).collect(Collectors.toList()));
 		assertEquals(2,storeService.findStoreByUser(user.getEmail()).size());
@@ -125,14 +145,14 @@ class StoreServiceTest {
 			User user = new User(1L, "UserName", "UserName@email.com", "Password", role, "1234567890", null, true);
 			Store store1 = new Store(1L, user, "StoreName", "StoreDescription");
 			Store store2 = new Store(2L, user, "StoreName", "StoreDescription");
-			
-			
+
+
 			@Override
 			public void execute() throws Throwable {
 				when(userService.getUser(user.getEmail())).thenReturn(Optional.of(user));
 				when(storeService.findStoreByUser(user.getEmail())).thenReturn(Stream.of(store1,store2).collect(Collectors.toList()));
 				assertEquals(2,storeService.findStoreByUser("username1@email.com").size());
-				
+
 			}});
 	}
 }
